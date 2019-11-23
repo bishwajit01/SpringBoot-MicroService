1daf6c128d91ad9c176662c13e78681d77f5d35f
bishwajit01 <bishwajit.vikram@gmail.com>
2019-11-23 22:25:03 +0530

    Initialized the three service

---
+++ rating-data-service/src/test/java/com/vikram/bishwajit/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.bishwajit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
