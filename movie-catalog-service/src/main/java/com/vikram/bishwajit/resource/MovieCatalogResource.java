e229f5a450be5323ac7ee517be73f6708fea9c9e
bishwajit01 <bishwajit.vikram@gmail.com>
2019-12-01 23:48:54 +0530

    talkign to the third Service.

--- movie-catalog-service/src/main/java/com/vikram/bishwajit/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/vikram/bishwajit/resource/MovieCatalogResource.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.vikram.bishwajit.model.CatalogItem;
 import com.vikram.bishwajit.model.Movie;
 import com.vikram.bishwajit.model.Rating;
+import com.vikram.bishwajit.model.UserRating;
 
 /**
  * @author Bishwajit.
@@ -23,26 +25,46 @@ import com.vikram.bishwajit.model.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 
 	@GetMapping("/all")
 	public List<CatalogItem> getCatalog() {
 		return Collections.singletonList(new CatalogItem("Bala", "About baldness", 4.3));
 	}
 
-	@GetMapping("/{userId}")
-	public List<CatalogItem> getCatalogByUserId(@PathVariable("userId") String id) {
+	@GetMapping("/restTemplate/{userId}")
+	public List<CatalogItem> getCatalogByUserIdUsingRestTemplate(@PathVariable("userId") String id) {
+		
+		UserRating userRating = restTemplate.getForObject("http://localhost:8883/ratings/user/" + id, UserRating.class);
+		
+		return userRating.getRatings().stream().map(
+				rating -> {
+					Movie movie = restTemplate.getForObject("http://localhost:8882/movies/" + rating.getMovieId(), Movie.class);
+					return new CatalogItem(movie.getMovieName(), "About AGENTS", rating.getRating());
+				}).collect(Collectors.toList());
 
+	}
+	
+	@GetMapping("/webClient/{userId}")
+	public List<CatalogItem> getCatalogByUserIdUsingWebClient(@PathVariable("userId") String id) {
 		// Get All rating movie id.
 		List<Rating> ratingList = Arrays.asList(new Rating("1", 4.5));
-
+		
+		// Web Client in place of REST TEMPLATE
+		// This is when we deal with asynchronous. 
 		return ratingList.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8882/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = webClientBuilder.build()
+							.get()  // Depends on the usage, GET POST PUT DELETE
+							.uri("http://localhost:8882/movies/" + rating.getMovieId())
+							.retrieve()
+							.bodyToMono(Movie.class)
+							.block(); // To block further more to execute the command, until it gets the values(esp. during the asynchronous).
 			return new CatalogItem(movie.getMovieName(), "About AGENTS", rating.getRating());
 		}).collect(Collectors.toList());
-
-		// return Collections.singletonList(new CatalogItem("Bala1", "About descrimination", 3.3));
 	}
 }
