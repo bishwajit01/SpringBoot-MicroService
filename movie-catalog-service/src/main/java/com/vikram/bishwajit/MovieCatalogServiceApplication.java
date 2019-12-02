5adaca15be9930f2998ca84ffbf97d3668c62523
bishwajit01 <bishwajit.vikram@gmail.com>
2019-12-02 20:46:59 +0530

    enabling the eureka client in the APIs.

--- movie-catalog-service/src/main/java/com/vikram/bishwajit/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/vikram/bishwajit/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.vikram.bishwajit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
--- rating-data-service/src/main/java/com/vikram/bishwajit/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/com/vikram/bishwajit/RatingDataServiceApplication.java
@@ -2,6 +2,7 @@ package com.vikram.bishwajit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingDataServiceApplication {
 
 	public static void main(String[] args) {
