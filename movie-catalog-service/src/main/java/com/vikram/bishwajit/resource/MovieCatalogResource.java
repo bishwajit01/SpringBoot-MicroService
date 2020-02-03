aa34e7d895d51b04f9dcc69a493c9d99d170f5fb
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-03 16:53:02 +0530

    Changed to the right discovery Client.

--- movie-catalog-service/src/main/java/com/vikram/bishwajit/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/vikram/bishwajit/resource/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.netflix.discovery.DiscoveryClient;
+
 import com.vikram.bishwajit.model.CatalogItem;
 import com.vikram.bishwajit.model.Movie;
 import com.vikram.bishwajit.model.Rating;
