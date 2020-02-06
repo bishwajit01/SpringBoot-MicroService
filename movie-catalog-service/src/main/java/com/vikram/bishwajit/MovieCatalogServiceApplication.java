e27345602c0cf955847b85fc20f358b3ed9cf819
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-06 19:21:26 +0530

    Adding Time out, in case of failure.

--- movie-catalog-service/src/main/java/com/vikram/bishwajit/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/vikram/bishwajit/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -24,9 +25,11 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpComponentsClientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(httpComponentsClientHttpRequestFactory);
 	}
-	
+
 	@Bean
 	public WebClient.Builder getWebClientBuilder() {
 		return WebClient.builder();
