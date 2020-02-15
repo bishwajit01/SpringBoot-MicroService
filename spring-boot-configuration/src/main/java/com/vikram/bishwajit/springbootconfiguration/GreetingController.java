3cdf6553a74f71b3a15d41f68f7574451a504338
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-15 19:20:20 +0530

    Learning about @Value annotation.

--- spring-boot-configuration/src/main/java/com/vikram/bishwajit/springbootconfiguration/GreetingController.java
+++ spring-boot-configuration/src/main/java/com/vikram/bishwajit/springbootconfiguration/GreetingController.java
@@ -1,6 +1,7 @@
 package com.vikram.bishwajit.springbootconfiguration;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +13,38 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingController {
-
+	
+	/**
+	 * We have to use ${}, to seek any value from properties files.
+	 * If key is not there, we can assign the default value, after the colon. 
+	 * If key is not found in properties file, it will throw an RunTime Exception.
+	 * Exception :: Error creating bean, Could not find the placeholder my.greeting.
+	 */
 	@Value("${my.greeting:default_values}")
 	private String greeting;
 
+	/**
+	 * If we give just the values without the ${},
+	 * Then the value will be assigned to the variable directly.
+	 */
 	@Value(" Bishwajit")
 	private String firstName;
-
+	
+	/**
+	 * This is for the list of values.
+	 */
 	@Value("${my.list.values}")
 	private List<String> listValues;
+	
+	/**
+	 * 
+	 * This is for the map values.
+	 */
+	@Value("#{${my.map.values}}")
+	private Map<String, String> mapValues;
 
 	@GetMapping("/greeting")
 	public String greet() {
-		return this.greeting + firstName + listValues;
+		return this.greeting + firstName + listValues + mapValues;
 	}
 }
