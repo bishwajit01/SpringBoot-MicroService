aeb520342079932d8fce6b539784eacff94235ac
bishwajit01 <bishwajit.vikram@gmail.com>
2020-02-17 23:53:22 +0530

    commting new config file v3.

--- spring-boot-configuration/src/main/java/com/vikram/bishwajit/springbootconfiguration/GreetingController.java
+++ spring-boot-configuration/src/main/java/com/vikram/bishwajit/springbootconfiguration/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@RefreshScope
 public class GreetingController {
 	
 	/**
